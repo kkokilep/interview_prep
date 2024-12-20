d7afe16e94ef5d6d4dc26e01d31867c25fa2a15d
Kiran <kkokilepersaud@gmail.com>
2024-12-20 05:56:48 -0500

    first

--- self_taught_computer/9_array.py
+++ self_taught_computer/9_array.py
@@ -13,3 +13,24 @@ def move_zeros(a_list):
                 a_list[index] = 0
             zero_index = zero_index + 1
     return (a_list)
+
+def return_duplicates(a_list):
+    dups = []
+    a_set = set()
+    for item in a_list:
+        l1 = len(a_set)
+        a_set.add(item)
+        l2 = len(a_set)
+        if(l1 == l2):
+            dups.append(item)
+    return dups
+
+def return_intersection(list1, list2):
+    list3 = [v for v in list1 if v in list2]
+    return list3
+
+def return_iter(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    return list(set1.intersection(set2))
+
